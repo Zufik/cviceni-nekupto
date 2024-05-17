1810c36ab7e08645e250e2d79c824c0bec851daa
Lenka Zufala <m-lenkazufala@seznam.cz>
2024-05-17 08:29:04 +0200

    ch

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const product1 = {
+const product = document.getElementById ("product1")
+console.log (product)
+
+/*const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
   description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
@@ -15,3 +18,13 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const elproduct1 = document.querySelector ("#product1")
+elproduct1.classList.add ("border-primary")
+
+const elButton = document.querySelector ("#product2 button")
+elButton.classList.remove ("btn-primary")
+
+const elTitle = document.querySelector("#product3 .card-title")
+elTitle.classList.toggle ("text-center")
+*/
